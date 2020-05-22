c451eedd7dd1a0e699cbcabc5041242e3d93977c
Shaik-bridgelabz <shaikmohammed395@gmail.com>
2020-05-23 00:30:23 +0530

    Added a new Class to set and get Person Details.

---
+++ src/main/java/com/bridgelabz/addressbook/PersonDetails.java
@@ -0,0 +1,81 @@
+package com.bridgelabz.addressbook;
+
+public class PersonDetails {
+
+    private String FirstName;
+    private String LastName;
+    private String Address;
+    private String City;
+    private String State;
+    private String Zip;
+    private String PhoneNumber ;
+
+    public String getFirstName() {
+        return FirstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public String getCity() {
+        return City;
+    }
+
+    public String getState() {
+        return State;
+    }
+
+    public String  getZip() {
+        return Zip;
+    }
+
+    public String getPhoneNumber() {
+        return PhoneNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        FirstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public void setCity(String city) {
+        City = city;
+    }
+
+    public void setState(String state) {
+        State = state;
+    }
+
+    public void setZip(String zip) {
+        Zip = zip;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        PhoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonDetails{" +
+                "FirstName='" + FirstName + '\'' +
+                ", LastName='" + LastName + '\'' +
+                ", Address='" + Address + '\'' +
+                ", City='" + City + '\'' +
+                ", State='" + State + '\'' +
+                ", Zip=" + Zip +
+                ", PhoneNumber=" + PhoneNumber +
+                '}';
+    }
+}
